be5e3bca6b7fa546f164215d15c26d1dd9a1e030
luodaxiao <48942383+luodaxiaoxi@users.noreply.github.com>
2022-09-21 13:00:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -30,9 +30,9 @@ export const config = {
     // 金山每日一句
     CIBA: true,
     // 每日一言
-    oneTalk: false,
+    oneTalk: true,
     // 土味情话(彩虹屁)
-    earthyLoveWords: false,
+    earthyLoveWords: true,
     // 朋友圈文案
     momentCopyrighting: false,
     // 毒鸡汤
@@ -41,7 +41,7 @@ export const config = {
     poetry: false,
 
     /** 星座运势 */
-    horoscope: false,
+    horoscope: true,
 
     /** 生日消息和节日消息 */
     birthdayMessage: true,
@@ -51,7 +51,7 @@ export const config = {
 
   // 每日一言的内容类型
   // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-  LITERARY_PREFERENCE: "",
+  LITERARY_PREFERENCE: "原创",
   
 
   /**
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "og-1B5wtTz_oNRYZ6MO4Wf0nGjWM",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "ibdOvWm0q8uTUYzqOuhNhC5YN_wA1Va4WwTD2_JuFBQ",
+      useTemplateId: "OrQDsj2mm3MA1YCrGS7OpnqGeONO88Oo2Dyy_sOQe6w",
       // 所在省份
       province: "江西", 
       // 所在城市
@@ -74,7 +74,7 @@ export const config = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn",
+      openUrl: "https://luodaxiaoxi.github.io/",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -95,9 +95,9 @@ export const config = {
       ]
     },
     {
-      name: "测试",
+      name: "luoxiao",
       id: "og-1B5yJE6FCvRoRY2U26x6CzI6Q",
-      useTemplateId:  "ibdOvWm0q8uTUYzqOuhNhC5YN_wA1Va4WwTD2_JuFBQ",
+      useTemplateId:  "OrQDsj2mm3MA1YCrGS7OpnqGeONO88Oo2Dyy_sOQe6w",
       province: "江苏",
       city: "无锡",
       horoscopeDate: '11-16',
